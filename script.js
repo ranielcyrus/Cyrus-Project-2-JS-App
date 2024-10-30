83fd4a2bbc5de5d2d1c9b600c167b8cfb3e8e641
ranielcyrus <ranielcyrus97@gmail.com>
2024-10-30 22:18:46 +0800

    fix: fix the orientations and the footer

--- script.js
+++ script.js
@@ -12,61 +12,87 @@ let mean = document.querySelector('.meaning')
 let read = document.querySelector('#read-btn')
 let close = document.querySelector('#close-btn')
 
+//variable to be able to access globally
+let varPhonetics = '';
+let varDefinition = '';
 
-//function 
+
+//API function 
 async function getWord() {
-try {
     //fetching dictionary api and adding the parameter on the last part
     let response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${inputField.value}`)
     let data = await response.json();
 
-    //DOM manipulation for Pages
-    //inputted word to show in page
-    result.textContent = `${inputField.value}`;
-    //checker for phonetics availability 
+    if (!response.ok) throw new Error('Network response was not ok.');
+
+    //checker for phonetics availability and assinging  assign
     if(data[0].phonetics[0].text!==undefined){
-        phonet.textContent = `Phonetic: ${data[0].phonetics[0].text}`;
+        varPhonetics = data[0].phonetics[0].text;
     } else {
-        phonet.textContent = `Phonetic: ${data[0].phonetics[1].text}`;
+        varPhonetics = data[0].phonetics[1].text;
     }
-    //meaning of the word
-    mean.textContent = `Definition: ${data[0].meanings[0].definitions[0].definition}`
 
-        ///read button
-        read.addEventListener("click", function () {
-            //text to speech the input word
-            speech.text = inputField.value
-            window.speechSynthesis.speak(speech);
-            //text to speech the meaning of the inputted word
-            speech.text = data[0].meanings[0].definitions[0].definition
-            window.speechSynthesis.speak(speech);
-        })
-
-        //close button
-        close.addEventListener("click", function() {
-            //stop the text to speech
-            window.speechSynthesis.cancel();
-            //close the book
-            book.style.transform = `translateX(${0}px)`;
-            frontCover.style.transform = `rotateY(${0}deg)`;
-            frontTitle.style.display='inline';
-        })
-
-    } catch(err) {
-        //show error
-        phonet.textContent = "Error: No definitions found"; 
-        //remove meaning text content if ever there's an exisiting one
-        mean.textContent = ""
-    }
+    //assing definition 
+    varDefinition=data[0].meanings[0].definitions[0].definition;
+    return varDefinition, varPhonetics;     
+    
 }
 
+//DOM manipulations function
+function domElements() {
+    //inputted word to show in page
+    result.textContent = `${inputField.value}`;
+    //phonetics show in page
+    phonet.textContent = `Phonetic: ${varPhonetics}`;
+    //meaning of the word
+    mean.textContent = `Definition: ${varDefinition}`
+
+    ///read button
+    read.addEventListener("click", function () {
+        // stop any ongoing speech
+        window.speechSynthesis.cancel();
+
+        //text to speech the input word
+        let toSpeak = result.textContent.concat(varDefinition) //combining the word and definiton
+        speech.text = toSpeak;
+        window.speechSynthesis.speak(speech);
+        //wait for the first speech to finish before stops the continuous speaking
+        speech.onend = () => {
+            window.speechSynthesis.cancel();
+        };
+    })
+
+    //close button
+    close.addEventListener("click", function() {
+        //stop the text to speech
+        window.speechSynthesis.cancel();
+        //close the book
+        book.style.transform = `translateX(${0}px)`;
+        frontCover.style.transform = `rotateY(${0}deg)`;
+        frontTitle.style.display='inline';
+    })
+}
+
+
 //event listener
-btn.addEventListener("click", function (){
+btn.addEventListener("click", async () => {
     //check if the input field is not empty, use trim to check for white spaces
     if(inputField.value.trim().length !== 0){
-        getWord();
+       
+        try {
+            //execute the call API and DOM manupulation functions
+            await getWord();
+            domElements();
 
-        //opening the book
+        } catch(err) {
+            //show error
+            result.textContent = `${inputField.value}`;
+            phonet.textContent = "Error: No definitions found"; 
+            //remove meaning text content if ever there's an exisiting one
+            mean.textContent = ""
+        }
+
+        //opening the book, check first for responsiveness
         if(window.matchMedia("(max-width: 480px)")){
             book.style.transform = `translateX(${80}px)`;
             frontCover.style.transform = `rotateY(${-150}deg)`;
@@ -74,14 +100,13 @@ btn.addEventListener("click", function (){
             result.style.fontSize= "1.5rem";
             phonet.style.fontSize= "0.75rem";
             mean.style.fontSize= "0.75rem";
-        }
-        else {
+        } else {
         book.style.transform = `translateX(${200}px)`;
         frontCover.style.transform = `rotateY(${-180}deg)`;
         frontTitle.style.display='none';
         }
-    } 
-    else {
+        
+    } else {
         inputField.setAttribute('placeholder', 'Empty Fields! Pls Enter a word')
         alert("Input field is empty")
     } 
